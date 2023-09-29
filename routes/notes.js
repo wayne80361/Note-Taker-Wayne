0e55914bbe7c4a6eb8583bce2f4cc4aa060164e8
wayne80361 <wayne80361@gmail.com>
2023-09-28 23:37:42 -0400

    instal npm uuid, and add post route for adding new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,34 @@
 const notes = require("express").Router();
 // import helper function
-const { readFromFile } = require("../helpers/fsUtils");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+// import unique ID generator
+const { v4: uuidv4 } = require("uuid");
 
-// GET Route for retrieving all the
+// GET Route for retrieving all the notes
 // api/notes
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+// api/notes
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully`);
+  } else {
+    res.error("Error in adding note");
+  }
 });
 
 module.exports = notes;
