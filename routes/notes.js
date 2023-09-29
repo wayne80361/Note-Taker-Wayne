aa3d4561c76e09a7a02114533bed402a6902bee0
wayne80361 <wayne80361@gmail.com>
2023-09-28 23:24:33 -0400

    add get all notes route, and read from db/db.json

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,11 @@
 const notes = require("express").Router();
+// import helper function
+const { readFromFile } = require("../helpers/fsUtils");
 
-notes.get("/", (req, res) => {});
+// GET Route for retrieving all the
+// api/notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 module.exports = notes;
