1905ba72dab4aa97ed1e4b43bda1c41e5df51eb5
wayne80361 <wayne80361@gmail.com>
2023-09-28 22:59:02 -0400

    add server.js with endPoint /, /api, /notes

---
+++ server.js
@@ -0,0 +1,31 @@
+// DEPENDENCIES
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index.js");
+
+// PORT/APP
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+// STATIC public folder
+app.use(express.static("public"));
+
+// ROUTES
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET Route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// START THE SERVER
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
