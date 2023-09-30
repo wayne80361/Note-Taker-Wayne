406b2aedb679fa996817f47911acf409a7dbad76
wayne80361 <wayne80361@gmail.com>
2023-09-30 15:59:06 -0400

    change * back to / for homepage, add delete route to notes.js, remove Test Title as it has no id for the delete function to work

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require("express").Router();
+const fs = require("fs");
 // import helper function
 const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 // import unique ID generator
@@ -33,6 +34,40 @@ notes.post("/", (req, res) => {
 
 // DELETE Route for an existing note
 // api/notes/:id
-notes.delete("/:note_id", (req, res) => {});
+notes.delete("/:note_id", (req, res) => {
+  // Get the note ID from the request parameters
+  const noteIdToDelete = req.params.note_id;
+
+  // Read the existing notes from the JSON file
+  readFromFile("./db/db.json")
+    .then((data) => {
+      const notes = JSON.parse(data);
+
+      // Find the index of the note with the specified ID
+      const noteIndex = notes.findIndex((note) => note.id === noteIdToDelete);
+
+      // Check if the note exists
+      if (noteIndex !== null) {
+        // Remove the note from the array
+        notes.splice(noteIndex, 1);
+
+        // Write the updated notes array back to the file, and use indentation of 4 for spacing
+        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json("Error deleting note");
+          } else {
+            res.json("Note deleted successfully");
+          }
+        });
+      } else {
+        res.status(404).json("Note not found");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json("Error reading notes");
+    });
+});
 
 module.exports = notes;
